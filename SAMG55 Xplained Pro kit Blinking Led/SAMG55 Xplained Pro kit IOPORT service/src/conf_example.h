d07db3e6f70675fda510ff886a47789bca8247c1
yomuevans <54433381+yomuevans@users.noreply.github.com>
2022-05-31 13:20:36 +0300

    Add files via upload

---
+++ SAMG55 Xplained Pro kit Blinking Led/SAMG55 Xplained Pro kit IOPORT service/src/conf_example.h
@@ -0,0 +1,20 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 5/31/2022 10:11:41 AM
+ *  Author: YOMU
+ */ 
+
+
+#ifndef CONF_EXAMPLE_H_INCLUDED
+#define CONF_EXAMPLE_H_INCLUDED
+
+/* Using LED0 SAMG55-XPLAINED-PRO (PA6) */
+#define EXAMPLE_LED_PORT (0)
+#define EXAMPLE_LED_MASK ((1 << 6))
+
+/* Using button SW0 on SAMG55-XPLAINED-PRO (PA02) */
+#define EXAMPLE_BUTTON_PORT (0)
+#define EXAMPLE_BUTTON_MASK ((1 << 2))
+
+#endif /* CONF_EXAMPLE_H_INCLUDED */
